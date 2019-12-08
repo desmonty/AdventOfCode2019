81569606a79f843a952b4b3ac3fcfed2616ef86d
Desmonty <charles.desmonty@itspeanutdata.com>
2019-12-08 16:27:46 -0500

    +test answer 2

--- Day5/answer.py
+++ Day5/answer.py
@@ -8,4 +8,24 @@ if __name__ == '__main__':
 
     print("Answer #5.1: ", output_1[-1])
 
+    program_is_8_position = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+    assert IntCode(program_is_8_position).run(inputs=[7]).get_output()[0] == 0
+    assert IntCode(program_is_8_position).run(inputs=[8]).get_output()[0] == 1
+    assert IntCode(program_is_8_position).run(inputs=[9]).get_output()[0] == 0
+
+    program_lt_8_position = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+    assert IntCode(program_lt_8_position).run(inputs=[7]).get_output()[0] == 1
+    assert IntCode(program_lt_8_position).run(inputs=[8]).get_output()[0] == 0
+    assert IntCode(program_lt_8_position).run(inputs=[9]).get_output()[0] == 0
+
+    program_is_8_immediate = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+    assert IntCode(program_is_8_immediate).run(inputs=[7]).get_output()[0] == 0
+    assert IntCode(program_is_8_immediate).run(inputs=[8]).get_output()[0] == 1
+    assert IntCode(program_is_8_immediate).run(inputs=[9]).get_output()[0] == 0
+
+    program_lt_8_immediate = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+    assert IntCode(program_lt_8_immediate).run(inputs=[7]).get_output()[0] == 1
+    assert IntCode(program_lt_8_immediate).run(inputs=[8]).get_output()[0] == 0
+    assert IntCode(program_lt_8_immediate).run(inputs=[9]).get_output()[0] == 0
+
     # print("Answer #5.2: ", )
